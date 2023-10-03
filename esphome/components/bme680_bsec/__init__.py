fc3cca0dcfbf4e47e260c20c984f8596c6c51c42
KuRue <ambpc.net@gmail.com>
2023-10-03 16:05:36 -0400

    Update __init__.py

--- esphome/components/bme680_bsec/__init__.py
+++ esphome/components/bme680_bsec/__init__.py
@@ -75,6 +75,6 @@ async def to_code(config):
     cg.add_library(
         "BSEC2 Software Library",
         #"v1.4.2200",
-        #"https://github.com/BoschSensortec/Bosch-BSEC2-Library.git",
-        "https://github.com/boschsensortec/Bosch-BSEC2-Library/tree/aeb4e4931fccdd8524c0b40d14aa2c5eec599ff6",
+        "https://github.com/BoschSensortec/Bosch-BSEC2-Library.git#v1.4.2200",
+        #"https://github.com/boschsensortec/Bosch-BSEC2-Library/tree/v1.4.2200",
     )
